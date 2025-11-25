068bba57592e5f157cc83be67f1a4a2ee6f969e8
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2025-11-25 08:42:07 -0300

    cadastrar-jogo-base

--- processamento/processamento.php
+++ processamento/processamento.php
@@ -9,6 +9,19 @@ function ConexaoBD()
 function InserirJogo()
 {
     $msqli = ConexaoBD();
+
+    $nome = $_POST['inputNome'];
+    $descricao = $_POST['inputDescricao'];
+    $preco_padrao = $_POST['inputPrecoPadrao'];
+    $preco_padrao = $_POST['inputPrecoDesconto'];
+
+    $sql_code = "INSERT INTO jogos (nome, descricao, preco_padrao, preco_desconto) VALUES ('{$nome}', '{$descricao}', '{$preco_padrao}', '{$preco_desconto}')";
+    $sql_query = $msqli->query($sql_code) or die("Falha na execucao do código SQL: " . $msqli->error);
+
+    echo "Jogo Cadastrado com sucesso.";
 }
+
+if ($_POST['acao'] == 'cadastrar')
+    InserirJogo();
 
 ?>
